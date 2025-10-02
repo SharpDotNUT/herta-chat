e89c13c48a57c4a74f2de2caaee8e80884a82827
Kuriyota <Kuriyota@outlook.com>
2025-10-02 21:56:39 +0800

    Release v0.1.0 with bug fix
    Introduced a GitHub Actions workflow for building and prereleasing on tag push. Updated root package version to 0.1.0, added 'es-toolkit' to root dependencies, and removed 'es-toolkit' and 'i' from frontend package dependencies. The app bar now displays the app version from package.json. Minor CSS update to set position: relative on #main. Updated lockfile to reflect dependency changes.

--- packages/herta-chat-frontend/src/App.vue
+++ packages/herta-chat-frontend/src/App.vue
@@ -1,10 +1,15 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
+import PackageJSON from '../../../package.json'
 </script>
 
 <template>
   <div id="app">
-    <var-app-bar> Herta Chat </var-app-bar>
+    <var-app-bar>
+      Herta Chat
+      <span>&nbsp;|&nbsp;</span>
+      <span> v{{ PackageJSON.version }} </span>
+    </var-app-bar>
     <RouterView id="container" />
   </div>
 </template>
